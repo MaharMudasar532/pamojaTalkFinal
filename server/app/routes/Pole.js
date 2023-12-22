b99b5c5609fdead7b2128b04a4d28d5eeb987eda
ihteshamm112 <125880962+ihteshamm112@users.noreply.github.com>
2023-12-22 22:51:22 +0500

    Update Pole.js

--- server/app/routes/Pole.js
+++ server/app/routes/Pole.js
@@ -5,10 +5,10 @@ const upload = require("../middlewares/FolderImagesMulter")
 
 let router = require("express").Router();
 
-router.post("/add", Pole.create);
+router.post("/add", upload.single('image'), Pole.create);
 router.post("/view_specific", Pole.viewSpecific);
 router.post("/view_all", Pole.viewAll);
-router.put("/update", Pole.update);
+router.put("/update",  upload.single('image'), Pole.update);
 router.delete("/delete" , Pole.delete)
 router.post("/search", Pole.search);
 // router.post("/view_all_with_item_count", Pole.view_all_with_item_count);
